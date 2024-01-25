1127573146f8e6636bf5c2b758613820cf76c25b
BajanVlogs <kjolly544@gmail.com>
2024-01-25 00:01:52 -0500

    Create EventListener.php

---
+++ src/BanSystem/EventListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BanSystem;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerLoginEvent;
+
+class EventListener implements Listener {
+
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+    }
+
+    /**
+     * @param PlayerLoginEvent $event
+     */
+    public function onPlayerLogin(PlayerLoginEvent $event) {
+        // Check if the player is banned and handle accordingly
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        if ($this->plugin->getBanManager()->isBanned($name)) {
+            $event->setCancelled(true);
+            $player->kick("You are banned from the server.");
+        }
+    }
+}
